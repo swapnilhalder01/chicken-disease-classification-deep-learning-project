b3b41d62ce04d413b4e4d71ae0608bfe483ad107
swapnilhalder01 <swapnilhalder701@gmail.com>
2025-05-21 00:16:50 +0600

    User and Prediction added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -6,6 +6,7 @@ from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
                                                 TrainingConfig,
+                                                EvaluationConfig
                                                 )
 
 
@@ -94,6 +95,18 @@ class ConfigurationManager:
         )
 
         return training_config
+    
+    
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
+
     
 
 
